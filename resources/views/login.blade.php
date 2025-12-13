88e84424e3aa4bd0e1401c866c072800edc15866
RizkaRslndP <rrizkar28@gmail.com>
2025-12-13 16:14:10 +0700

    rizka
    update 16.14

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -29,15 +29,11 @@
         </div>
       </div>
     </div>
-    <div aria-hidden="true" class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-      <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"></div>
-    </div>
-  </div>
-</div>
 
+    <!-- Login -->
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img src="{{ Vite::asset('resources/assets/Logo-IAI-AL-AZIS.png') }}" alt="Your Company" class="mx-auto h-20 w-auto" />
+    <img src="{{ Vite::asset('resources/assets/Logo-IAI-AL-AZIS.png') }}" alt="IAI AL-AZIZ" class="mx-auto h-20 w-auto" />
     <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
   </div>
 
@@ -72,6 +68,8 @@
       <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Start a 14 day free trial</a>
     </p>
   </div>
+  </div>
+</div>
 </div>
 </body>
 </html>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -12,9 +12,9 @@
   <header class="absolute inset-x-0 top-0 z-50">
     <nav aria-label="Global" class="flex items-center justify-between p-6 lg:px-8">
       <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Your Company</span>
-          <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" class="h-8 w-auto" />
+        <a href="/dashboard" class="-m-1.5 p-1.5">
+          <span class="sr-only">IAI AL-AZIZ</span>
+          <img src="{{ Vite::asset('resources/assets/Logo-IAI-AL-AZIS.png') }}" alt="" class="h-10 w-auto" />
         </a>
       </div>
       <div class="flex lg:hidden">
@@ -40,9 +40,9 @@
         <div tabindex="0" class="fixed inset-0 focus:outline-none">
           <el-dialog-panel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
             <div class="flex items-center justify-between">
-              <a href="#" class="-m-1.5 p-1.5">
-                <span class="sr-only">Your Company</span>
-                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" class="h-8 w-auto" />
+              <a href="/dashboard" class="-m-1.5 p-1.5">
+                <span class="sr-only">IAI AL-AZIZ</span>
+                <img src="{{ Vite::asset('resources/assets/Logo-IAI-AL-AZIS.png') }}" alt="" class="h-10 w-auto" />
               </a>
               <button type="button" command="close" commandfor="mobile-menu" class="-m-2.5 rounded-md p-2.5 text-gray-200">
                 <span class="sr-only">Close menu</span>
