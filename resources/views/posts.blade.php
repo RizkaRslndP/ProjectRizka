a06aee626e39bd97236a4d6f8a963c0621847d24
RizkaRslndP <rrizkar28@gmail.com>
2025-12-17 10:42:51 +0700

    rizka
    update to much

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <article class="py-8 mx-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+        <div class="text-base text-gray-500"><a href="#">Rizka Rosalinda Pratiwi</a> | 1 January 2025</div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac vestibulum erat.
+            Cras vulputate auctor lectus at consequat. Donec in libero euismod, cursus nunc at, fermentum massa.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 mx-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+        <div class="text-base text-gray-500"><a href="#">Rizka Rosalinda Pratiwi</a> | 1 January 2025</div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae, non voluptates placeat impedit sequi nam, quod sapiente voluptate, possimus inventore ratione eligendi! Maxime velit aperiam est earum, minus ducimus.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,38 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
-    return view('login');
+    return view('login' , ['title' => 'Login Page']);
 });
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Rizka Rosalinda']);
+    return view('about', ['title' => 'About Us']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rizka Rosalinda Pratiwi',
+            'date' => '1 January 2025',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac vestibulum erat. Cras vulputate auctor lectus at consequat. Donec in libero euismod, cursus nunc at, fermentum massa.'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rizka Rosalinda Pratiwi',
+            'date' => '1 January 2025',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae, non voluptates placeat impedit sequi nam, quod sapiente voluptate, possimus inventore ratione eligendi! Maxime velit aperiam est earum, minus ducimus.'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact Us']);
 });
 
 Route::get('/edit-profil', function () {
-    return view('edit-profil');
+    return view('edit-profil', ['title' => 'Edit Profile']);
 });
